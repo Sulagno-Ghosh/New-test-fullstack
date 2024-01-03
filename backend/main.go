29c7c51c05016d68037207ea1ef4787986803742
Sulagno-Ghosh <sulagnoghosh889@gmail.com>
2024-01-03 17:46:09 +0530

    changes to the code format

--- backend/main.go
+++ backend/main.go
@@ -1,193 +1,206 @@
 package main
 
-import (
-	"database/sql"
-	"encoding/json"
-	"log"
-	"net/http"
-	"os"
+import(
+  "database/sql"
+  "encoding/json"
+  "log"
+  "net/http"
+  "os"
 
-	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
+  "github.com/gorilla/mux"
+  _ "github.com/lib/pq"
 )
 
+
 type User struct {
-	Id    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-
-func main() {
-
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	router := mux.NewRouter()
-	router.HandleFunc("/api/go/users", getUsers(db)).Methods("GET")
-	router.HandleFunc("/api/go/users", createUser(db)).Methods("POST")
-	router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("GET")
-	router.HandleFunc("/api/go/users/{id}", updateUser(db)).Methods("PUT")
-	router.HandleFunc("/api/go/users/{id}", deleteUser(db)).Methods("DELETE")
-
-	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
-
-	log.Fatal(http.ListenAndServe(":8080", enhancedRouter))
+  Id int `json:"id"`
+  Name string `json:"name"`
+  Email string `json:"email"`
 
 }
 
-func enableCORS(next http.Handler) http.Handler {
+func main(){
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+    if err != nil {
+      log.Fatal(err)
+    }
+    defer db.Close()
 
-		next.ServeHTTP(w, r)
 
-	})
+    _, err = db.Exec("CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
+
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    router := mux.NewRouter()
+    router.HandleFunc("/api/go/users", getUsers(db)).Methods("GET")
+    router.HandleFunc("/api/go/users", createUser(db)).Methods("POST")
+    router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("GET")
+    router.HandleFunc("/api/go/users/{id}", updateUser(db)).Methods("PUT")
+    router.HandleFunc("/api/go/users/{id}", deleteUser(db)).Methods("DELETE")
+
+    enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
+
+    log.Fatal(http.ListenAndServe(":8080", enhancedRouter))
 
 }
+
+
+
+ func enableCORS(next http.Handler) http.Handler {
+
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+        if r.Method == "OPTIONS" {
+          w.WriteHeader(http.StatusOK)
+          return
+        }
+
+        next.ServeHTTP(w, r)
+
+      })
+
+}
+
 
 func jsonContentTypeMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "application/json")
 
-		next.ServeHTTP(w, r)
-	})
+        next.ServeHTTP(w, r)
+    })
 
 }
 
+
 func getUsers(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
 
-		rows, err := db.Query("SELECT * FROM users")
+    rows, err := db.Query("SELECT * FROM users")
 
-		if err != nil {
-			log.Fatal(err)
-		}
+    if err != nil {
+      log.Fatal(err)
+    }
 
-		defer rows.Close()
+    defer rows.Close()
 
-		users := []User{}
+    users := []User{}
 
-		for rows.Next() {
-			var u User
+    for rows.Next() {
+      var u User
 
-			err := rows.Scan(&u.Id, &u.Name, &u.Email)
+      err := rows.Scan(&u.Id, &u.Name, &u.Email)
 
-			if err != nil {
-				log.Fatal(err)
-			}
+      if err != nil {
+        log.Fatal(err)
+      }
 
-			users = append(users, u)
-		}
+      users = append(users, u)
+    }
 
-		if err := rows.Err(); err != nil {
-			log.Fatal(err)
-		}
+    if err := rows.Err(); err != nil {
+      log.Fatal(err)
+    }
 
-		json.NewEncoder(w).Encode(users)
-	}
+    json.NewEncoder(w).Encode(users)
+  }
 }
 
 func getUser(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
 
-		vars := mux.Vars(r)
-		id := vars["id"]
+    vars := mux.Vars(r)
+    id := vars["id"]
 
-		var u User
+    var u User
 
-		err := db.QueryRow("SELECT * FROM users WHERE id=$1", id).Scan(&u.Id, &u.Name, &u.Email)
+    err := db.QueryRow("SELECT * FROM users WHERE id=$1", id).Scan(&u.Id, &u.Name, &u.Email)
 
-		if err != nil {
-			log.Fatal(err)
-		}
+    if err != nil {
+      log.Fatal(err)
+    }
 
-		json.NewEncoder(w).Encode(u)
-	}
+    json.NewEncoder(w).Encode(u)
+  }
 }
 
 func createUser(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
 
-		var u User
+    var u User
 
-		json.NewDecoder(r.Body).Decode(&u)
+    json.NewDecoder(r.Body).Decode(&u)
 
-		err := db.QueryRow("INSERT INTO users(name, email) VALUES($1, $2) RETURNING id", u.Name, u.Email).Scan(&u.Id)
+    err := db.QueryRow("INSERT INTO users(name, email) VALUES($1, $2) RETURNING id", u.Name, u.Email).Scan(&u.Id)
 
-		if err != nil {
-			log.Fatal(err)
-		}
+    if err != nil {
+      log.Fatal(err)
+    }
 
-		json.NewEncoder(w).Encode(u)
-	}
+    json.NewEncoder(w).Encode(u)
+  }
 
 }
 
 func updateUser(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+  return func(w http.ResponseWriter, r *http.Request) {
 
-		var u User
-		json.NewDecoder(r.Body).Decode(&u)
+    var u User
+    json.NewDecoder(r.Body).Decode(&u)
 
-		vars := mux.Vars(r)
-		id := vars["id"]
+    vars := mux.Vars(r)
+    id := vars["id"]
 
-		_, err := db.Exec("UPDATE users SET name=$1, email=$2 WHERE id=$3", u.Name, u.Email, id)
-		if err != nil {
-			log.Fatal(err)
-		}
+    _, err := db.Exec("UPDATE users SET name=$1, email=$2 WHERE id=$3", u.Name, u.Email, id)
+    if err!= nil {
+      log.Fatal(err)
+    }
 
-		var updateUser User
-		err = db.QueryRow("SELECT id,name,email FROM users WHERE id = $1", id).Scan(&updateUser.Id, &updateUser.Name, &updateUser.Email)
 
-		if err != nil {
-			log.Fatal(err)
-		}
+    var updateUser User
+    err = db.QueryRow("SELECT id,name,email FROM users WHERE id = $1", id).Scan(&updateUser.Id, &updateUser.Name, &updateUser.Email)
 
-		json.NewEncoder(w).Encode(updateUser)
+    if err != nil {
+      log.Fatal(err)
+    }
 
-	}
+
+    json.NewEncoder(w).Encode(updateUser)
+
+  }
 }
+
 
 func deleteUser(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id := vars["id"]
+    return func(w http.ResponseWriter, r *http.Request) {
+      vars := mux.Vars(r)
+      id := vars["id"]
 
-		var u User
-		err := db.QueryRow("SELECT * FROM users WHERE id=$1", id).Scan(&u.Id, &u.Name, &u.Email)
+      var u User
+      err := db.QueryRow("SELECT * FROM users WHERE id=$1", id).Scan(&u.Id, &u.Name, &u.Email)
 
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-		} else {
-			_, err := db.Exec("DELETE FROM users WHERE id=$1", id)
+      if err != nil {
+        w.WriteHeader(http.StatusNotFound)
+      }else {
+        _, err := db.Exec("DELETE FROM users WHERE id=$1", id)
 
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-		}
+        if err != nil {
+          w.WriteHeader(http.StatusInternalServerError)
+          return
+      }
+      }
 
-		json.NewEncoder(w).Encode("User deleted successfully")
+    json.NewEncoder(w).Encode("User deleted successfully")
 
-	}
+
+  }
 
 }
+
+
